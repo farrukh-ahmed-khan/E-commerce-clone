99cee7e475ccff192a3c8f25b413166c6c0371c8
Farrukh Ahmed Khan <khanfarrukh200@gmail.com>
2022-01-29 00:32:02 +0500

    E-commerce clone

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data={
+    product:[
+        {
+            _id: '1',
+            name:'nike slim shirt',
+            category: 'shirt',
+            image:'/image/p1.jpg',
+            brand: 'nike',
+            rating:4.5,
+            numReviews:10,
+            price:170,
+            countInStock:10,
+            desciription: 'high quality product'
+        },
+        {
+            _id: '2',
+            name:'adidas slim shirt',
+            category: 'shirt',
+            image:'/image/p2.jpg',
+            brand: 'adidas',
+            rating:4.0,
+            price:170,
+            countInStock:7,
+            numReviews:17,
+            desciription: 'high quality product'
+        },
+        {
+            _id: '3',
+            name:'puma slim shirt',
+            category: 'shirt',
+            image:'/image/p3.jpg',
+            brand: 'puma',
+            rating:4.3,
+            price:170,
+            countInStock:9,
+            numReviews:19,
+            desciription: 'high quality product'
+        },
+        {
+            _id: '4',
+            name:'nike slim pant',
+            category: 'pant',
+            image:'/image/p4.jpg',
+            brand: 'nike',
+            rating:4.5,
+            price:170,
+            countInStock:0,
+            numReviews:7,
+            desciription: 'high quality product'
+        },
+        {
+            _id: '5',
+            name:'nike slim pant',
+            category: 'pant',
+            image:'/image/p5.jpg',
+            brand: 'nike',
+            rating:4.8,
+            price:170,
+            countInStock:10,
+            numReviews:9,
+            desciription: 'high quality product'
+        },
+        {
+            _id: '6',
+            name:'lacoste slim pant',
+            category: 'pant',
+            image:'/image/p6.jpg',
+            brand: 'lacoste',
+            rating:4.8,
+            numReviews:12,
+            price:170,
+            countInStock:20,
+            desciription: 'high quality product'
+        },
+    ]
+}
+export default data;
